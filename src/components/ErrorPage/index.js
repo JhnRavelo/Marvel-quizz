f4f79e6a883f1deab20e4ae656e11e75ed123451
JhnRavelo <johnravelo135@gmail.com>
2023-08-14 16:51:14 +0300

    Routes terminé

---
+++ src/components/ErrorPage/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function ErrorPage() {
+  return (
+    <div className="quiz-bg">
+        <div className="container">
+            ERROR PAGE
+        </div>
+    </div>
+  )
+}
+
+export default ErrorPage
